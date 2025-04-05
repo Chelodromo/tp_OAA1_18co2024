0c2a5b63e4e3a852cb7435d19ebd3600962ba0dd
cptalamilla <ing.cristian.salinas@gmail.com>
2025-04-05 12:08:13 -0300

    corregido el checkeo del bucket antes de crearlo

--- dags/ml_dust_pred_dag.py
+++ dags/ml_dust_pred_dag.py
@@ -3,7 +3,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 import pandas as pd
-
+from botocore.exceptions import ClientError
 import os
 
 ## Minio bucket
@@ -13,10 +13,25 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime
 
 def ejemplo_conexion_s3():
+    bucket_name = 'respaldo'
     hook = S3Hook(aws_conn_id='minio_s3')
     s3_client = hook.get_conn()
-    s3_client.create_bucket(Bucket='respaldo')
-    s3_client.put_object(Bucket='respaldo', Key='prueba.txt', Body='Desde Airflow por variable de entorno')
+    # Verificar si el bucket ya existe
+    if not hook.check_for_bucket(bucket_name):
+        try:
+            s3_client.create_bucket(Bucket=bucket_name)
+            print(f"🪣 Bucket '{bucket_name}' creado correctamente.")
+        except ClientError as e:
+            print(f"❌ Error al crear bucket: {e}")
+    else:
+        print(f"✅ El bucket '{bucket_name}' ya existe. No se necesita crear.")
+    # Subir archivo de prueba
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key='prueba.txt',
+        Body='Desde Airflow por variable de entorno'
+    )
+    print(f"📄 Archivo 'prueba.txt' subido a bucket '{bucket_name}'.")
 
 
 
