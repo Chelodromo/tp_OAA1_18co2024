20bde66f6be5dbc415e2c88c01aaf31bfa70e3d5
Eze <111240250+ezemaudet@users.noreply.github.com>
2025-04-26 22:24:42 -0500

    Update main.py passworderror

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import os
 
 # --- 🔧 Configuración fija para MinIO
 minio_endpoint = os.getenv("MINIO_ENDPOINT", "localhost:9000")
-minio_access_key = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
-minio_secret_key = os.getenv("MINIO_SECRET_KEY", "minioadmin")
+minio_access_key = os.getenv("MINIO_ACCESS_KEY", "minio_admin")
+minio_secret_key = os.getenv("MINIO_SECRET_KEY", "minio_admin")
 bucket_name = "respaldo"
 modelo_key = "respaldo/modelos/modelo_svm.pkl"
 
